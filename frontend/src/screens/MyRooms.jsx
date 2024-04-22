b1ffa48e29ea14fa64b21dcf9cb4a9978e4ddc96
A-Anirudh <aayanamanirudh@gmail.com>
2024-04-22 18:25:01 +0530

    my meetings button added to end meeting

--- frontend/src/screens/MyRooms.jsx
+++ frontend/src/screens/MyRooms.jsx
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import { useAllMyRoomsQuery } from '../slices/roomSlice'
+import { useAllMyRoomsQuery,useEndRoomMutation } from '../slices/roomSlice'
+import { toastSuccess } from '../utils/myToast'
 
 export const MyRooms = () => {
     const [rooms, setRooms] = useState([])
     const [success, setSuccess] = useState(false)
     const { data, error, refetch } = useAllMyRoomsQuery()
+    const [endMeeting] = useEndRoomMutation() 
+
+    const handleEndMeeting = async (roomId) => {
+        console.log(roomId)
+        const resultData = await endMeeting({roomId})
+        await refetch()
+
+        if(resultData){
+            console.log(resultData)
+            toastSuccess('Meeting ended successfully!')
+        } else{
+            console.log('error')
+        }
+    }
 
     useEffect(() => {
         if (data) {
             setRooms(data.data)
             setSuccess(data.success)
         }
-
     }, [data])
-
     return (
         <>
             <div>My rooms</div>
@@ -25,6 +38,8 @@ export const MyRooms = () => {
                         <ul  className='mx-10 border w-[250px]'>
                             <li>Room name: {room.roomName}</li>
                             <li>Room ID: {room.roomId}</li>
+                            {room.isActive ? <li><button onClick={() => handleEndMeeting(room.roomId)}>End meeting</button></li> : '' }
+                            
                         </ul>
                         <br />
                     </div>
