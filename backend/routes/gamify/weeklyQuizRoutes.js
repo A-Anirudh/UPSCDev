49a50aca8269573fdcfa25fb9f21c1481be69b37
A-Anirudh <aayanamanirudh@gmail.com>
2024-04-25 23:40:14 +0530

    changed little code of weekly quiz

--- backend/routes/gamify/weeklyQuizRoutes.js
+++ backend/routes/gamify/weeklyQuizRoutes.js
@@ -7,13 +7,13 @@ import isAdmin from "../../middleware/adminMiddleware.js";
 const weeklyQuestionsRoute = express.Router();
 
 weeklyQuestionsRoute.get('/questions/all',protect, getQuestion);
-weeklyQuestionsRoute.post('/questions/new', protect,addQuestion);
+weeklyQuestionsRoute.post('/questions/new', protect,isAdmin,addQuestion);
 weeklyQuestionsRoute.delete('/questions/delete', protect,deleteQuestion);
 weeklyQuestionsRoute.get('/questions/solutions', protect, getSolution);
 weeklyQuestionsRoute.get('/questions/leaderboard', protect, leaderboard);
 weeklyQuestionsRoute.get('/questions/checkAttempted', protect, checkAttempted);
 
-weeklyQuestionsRoute.post('/solutions/new', protect,isAdmin,addSolution);
+weeklyQuestionsRoute.post('/solutions/new', protect,addSolution);
 
 
 export default weeklyQuestionsRoute;
